83af163e76e223f78efdcef70aa771eeddeeefcd
cpm <162544069+cpm-code@users.noreply.github.com>
2026-05-20 21:32:19 +0800

    kodi: bump cpm-code pin

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="0f67b94ff4b86c491df039376e3500661213fae3"
+PKG_VERSION="b5b3eb1f9b66b3bffe01a6a9dfd13228663eb640"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
